d63dfe08a097ff31a7f46d6e4c0e269a85e25e72
Alvar Hansen <alluxxii@gmail.com>
2014-06-17 15:38:58 +0300

    adds purchase_date_ms, expires_date_ms and in_app attribute. in_app is array with Receipt objects. Moved original attributes to receipt attributes, away from original_attributes hash.

--- lib/venice/receipt.rb
+++ lib/venice/receipt.rb
@@ -13,6 +13,7 @@ module Venice
 
     # The date and time this transaction occurred. This value corresponds to the transaction’s transactionDate property.
     attr_reader :purchase_date
+    attr_reader :purchase_date_ms
 
     # A string that the App Store uses to uniquely identify the application that created the payment transaction. If your server supports multiple applications, you can use this value to differentiate between them. Applications that are executing in the sandbox do not yet have an app-item-id assigned to them, so this key is missing from receipts created by the sandbox.
     attr_reader :app_item_id
@@ -28,6 +29,9 @@ module Venice
 
     # For a transaction that restores a previous transaction, this is the original receipt
     attr_accessor :original
+    attr_reader :original_transaction_id
+    attr_reader :original_purchase_date
+    attr_reader :original_purchase_date_ms
 
     # For an active subscription was renewed with transaction that took place after the receipt your server sent to the App Store, this is the latest receipt.
     attr_accessor :latest
@@ -37,12 +41,16 @@ module Venice
 
     # For auto-renewable subscriptions, returns the date the subscription will expire
     attr_reader :expires_at
+    attr_reader :expires_date_ms
+
+    attr_accessor :in_app
 
     def initialize(attributes = {})
       @quantity = Integer(attributes['quantity']) if attributes['quantity']
       @product_id = attributes['product_id']
       @transaction_id = attributes['transaction_id']
       @purchase_date = DateTime.parse(attributes['purchase_date']) if attributes['purchase_date']
+      @purchase_date_ms = Integer(attributes['purchase_date_ms']) if attributes['purchase_date_ms']
       @app_item_id = attributes['app_item_id']
       @version_external_identifier = attributes['version_external_identifier']
       @bid = attributes['bid']
@@ -50,31 +58,60 @@ module Venice
 
       # expires_date is in ms since the Epoch, Time.at expects seconds
       @expires_at = Time.at(attributes['expires_date'].to_i / 1000) if attributes['expires_date']
+      @expires_date_ms = Integer(attributes['expires_date']) if attributes['expires_date']
 
-      if attributes['original_transaction_id'] || attributes['original_purchase_date']
-        original_attributes = {
-          'transaction_id' => attributes['original_transaction_id'],
-          'purchase_date' => attributes['original_purchase_date']
-        }
+      @original_transaction_id = attributes['original_transaction_id']
+      @original_purchase_date = DateTime.parse(attributes['original_purchase_date']) if attributes['original_purchase_date']
+      @original_purchase_date_ms = Integer(attributes['original_purchase_date_ms']) if attributes['original_purchase_date_ms']
 
-        self.original = Receipt.new(original_attributes)
+      if attributes['in_app']
+        self.in_app = []
+        attributes['in_app'].each do |in_app_receipt_attributes|
+          self.in_app << Receipt.new(in_app_receipt_attributes)
+        end
       end
     end
 
-    def to_h
-      {
-        :quantity => @quantity,
-        :product_id => @product_id,
-        :transaction_id => @transaction_id,
-        :purchase_date => (@purchase_date.httpdate rescue nil),
-        :original_transaction_id => (@original.transaction_id rescue nil),
-        :original_purchase_date => (@original.purchase_date.httpdate rescue nil),
-        :app_item_id => @app_item_id,
-        :version_external_identifier => @version_external_identifier,
-        :bid => @bid,
-        :bvrs => @bvrs,
-        :expires_at => (@expires_at.httpdate rescue nil)
-      }
+    def in_app_product_identifiers
+      identifies = []
+      @in_app.each do |receipt|
+        identifies << receipt.product_id
+      end
+      identifies
+    end
+
+    def to_h(attributes={})
+      if attributes.any?
+        hash = {}
+        attributes[:include].each do |key|
+          value = self.send key
+          if value
+            hash[key] = self.send key
+          end
+        end
+        if @in_app
+          hash[:in_app] = []
+          @in_app.each do |receipt|
+            hash[:in_app] << receipt.to_h(attributes)
+          end
+        end
+        return hash
+      else
+        return {
+          :quantity => @quantity,
+          :product_id => @product_id,
+          :transaction_id => @transaction_id,
+          :purchase_date_ms => @purchase_date_ms,
+          :original_transaction_id => @original_transaction_id,
+          :original_purchase_date_ms => @original_purchase_date_ms,
+          :app_item_id => @app_item_id,
+          :version_external_identifier => @version_external_identifier,
+          :bid => @bid,
+          :bvrs => @bvrs,
+          :expires_date_ms => @expires_date_ms,
+          :in_app => @in_app
+        }
+      end
     end
 
     def to_json
